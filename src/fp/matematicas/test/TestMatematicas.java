39945b371f485dba5d0c07612e43930e7753d781
Jorge García <jorgarcia@us.es>
2023-02-07 21:49:56 +0100

    Cambios en el código y nuevas traspas

--- src/fp/matematicas/test/TestMatematicas.java
+++ src/fp/matematicas/test/TestMatematicas.java
@@ -4,9 +4,21 @@ import fp.matematicas.util.*;
 
 public class TestMatematicas {
 
-	public static void main(String[] args) {
-		
+	public static void testEsPar() {
 		System.out.println(Matematicas.esPar(32));
+	}
+
+	public static void testGeneraLista() {
+		System.out.println(Matematicas.generaLista(10));
+	}
+
+	public static void testGeneraListaSinRepetidos() {
+		System.out.println(Matematicas.generaListaSinRepetidos(10));
+	}
+
+	public static void main(String[] args) {
+		testGeneraLista();
+		testGeneraListaSinRepetidos();
 	}
 
 }
--- src/fp/matematicas/util/Matematicas.java
+++ src/fp/matematicas/util/Matematicas.java
@@ -1,8 +1,53 @@
 package fp.matematicas.util;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 public class Matematicas {
 
 	public static Boolean esPar(Integer i) {
-		return i%2==0;
+		return i % 2 == 0;
+	}
+
+	public static List<Integer> elevaAExponente(List<Integer> l, Integer exponente) {
+		return null;
+	}
+
+	public static List<Integer> filtraPrimos(List<Integer> l) {
+		return null;
+	}
+
+	public static List<Integer> getPares(List<Integer> valores) {
+		List<Integer> result = new ArrayList<>();
+
+		for (Integer numero : valores) {
+			if (esPar(numero)) {
+				result.add(numero);
+			}
+		}
+
+		return result;
+	}
+
+	public static List<Integer> generaLista(Integer n) {
+		List<Integer> result = new ArrayList<Integer>();
+		Random r = new Random();
+		for (int i = 0; i < n; i++) {
+			result.add(r.nextInt());
+		}
+		return result;
+	}
+
+	public static List<Integer> generaListaSinRepetidos(Integer n) {
+		Set<Integer> conjunto = new HashSet<>(generaLista(n));
+		Random r = new Random();
+		while (conjunto.size() < n) {
+			conjunto.add(r.nextInt());
+		}
+		List<Integer> result = new ArrayList<>(conjunto);
+		return result;
 	}
 }
